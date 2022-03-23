07b66205c42bcd99a9aade67f73cf843939b9b34
JWHCode <45348787+JWHCode@users.noreply.github.com>
2022-03-23 15:40:55 -0400

    Update python.py

--- python.py
+++ python.py
@@ -7,3 +7,15 @@ def summation(num):
         return total
     else:
         return 1
+
+#In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+#If anything in the text isn't a letter, ignore it and don't return it.
+
+def alphabet_position(text):
+    returnString = ""
+    for x in text:
+        if str.isalpha(x):
+            returnString += str(ord(x.lower())-96) + " "
+        else:
+            pass
+    return returnString.strip()
